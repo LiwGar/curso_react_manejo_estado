ce059e55019acb4914ca1ed9a41e21eb8a53b4de
liwgar <wilenri341@hotmail.com>
2024-10-01 13:37:02 -0500

    React.Component vs. useState implementation respectively

--- src/components/ClassState.js
+++ src/components/ClassState.js
@@ -1,24 +1,40 @@
 import React from "react";
 
 class ClassState extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+    }
+  };
+
   render() {
     return (
       <div>
+
         <h1>
-          ELiminar ClassState
+          Eliminar {this.props.name}
         </h1>
+
         <p>
           Por Favor, escribe el código de seguridad.
         </p>
+
+        {this.state.error && (
+        <p>Error: El código es incorrecto</p>
+        )}
+
         <input 
           placeholder="Codigo de seguridad"/>
 
-        <button>
+        <button
+          onClick={() => this.setState({error: !this.state.error})}>
           Comprobar
           </button>
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 export default ClassState;
--- src/components/UseState.js
+++ src/components/UseState.js
@@ -1,20 +1,33 @@
-function UseState() {
+import React from "react";
+
+function UseState({name}) {
+
+  const [error, setError] = React.useState(false);
+
   return (
     <div>
+
       <h1>
-        ELiminar UseState
+        Eliminar {name}
       </h1>
+
       <p>
         Por Favor, escribe el código de seguridad.
       </p>
+
+      {error && (
+        <p>Error: El código es incorrecto</p>
+      )}
+
       <input 
         placeholder="Codigo de seguridad"/>
 
-      <button>
+      <button
+        onClick={() => setError(!error)}>
         Comprobar
         </button>
     </div>
-  )
-}
+  );
+};
 
 export default UseState;
