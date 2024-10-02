7ff0ef341d5ab3125f03212acfa0ead555a219c0
liwgar <wilenri341@hotmail.com>
2024-10-02 13:02:07 -0500

    Implementation of useEffect and setTimeOut in the UseState component.

--- src/components/ClassState.js
+++ src/components/ClassState.js
@@ -3,10 +3,13 @@ import React from "react";
 class ClassState extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props); //Se llama a la funcion super() Cuando querramos modificar this.state
+    //en una clase, pero queremos que todo lo que haga hecho la clase que estamos extendiendo 
+    //siga funcionando. enviando todas las props.
+    
     this.state = {
       error: false,
-    }
+    };
   };
 
   render() {
--- src/components/UseState.js
+++ src/components/UseState.js
@@ -3,6 +3,23 @@ import React from "react";
 function UseState({name}) {
 
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("Empezando el efecto");
+
+    if(!!loading) {
+      setTimeout(() => {
+        console.log("Iniciando Validacion");
+  
+        setLoading(false)
+  
+        console.log("Finalizando Validacion");
+      }, 3000)
+    }
+
+    console.log("Terminando el efecto");
+  }, [loading]);
 
   return (
     <div>
@@ -19,11 +36,15 @@ function UseState({name}) {
         <p>Error: El código es incorrecto</p>
       )}
 
+      {loading && (
+        <p>Cargando...</p>
+      )}
+
       <input 
         placeholder="Codigo de seguridad"/>
 
       <button
-        onClick={() => setError(!error)}>
+        onClick={() => setLoading(true)}>
         Comprobar
         </button>
     </div>
